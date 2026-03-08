51279e8cc08cd99e4371208ec2aa0bbbcd25e989
unknown <pozniakhubert52@gmail.com>
2026-03-08 13:53:03 +0100

    added bluetooth volume control

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include "Arduino.h"
-
+#include "BluetoothA2DP.h"
 
 #define POT_VCC 33
 #define POT_SIG 32
+
+BluetoothA2DPSink a2dp_sink;
+A2DPLinearVolumeControl vc;
 
 const int leds[] = {13,12,14,27,26,25};
 
@@ -27,12 +30,17 @@ void setup()
 
   pinMode(POT_VCC, OUTPUT);
   digitalWrite(POT_VCC, HIGH);
-
+  a2dp_sink.set_volume_control(&vc);
+  a2dp_sink.start("Regler");
 }
 
 void loop()
 {
+
   if (millis() - last_time >= delay_ms) {
+    int volume = (((averaged_value - 0) * 127 / 4096) + 0);
+    if (volume > 127) volume = 127;
+
     int raw_value = analogRead(POT_SIG);
 
     averaged_value = (averaged_value * (average_count - 1) + raw_value) /average_count;
@@ -50,10 +58,10 @@ void loop()
       analogWrite(leds[i], 255);
     }
 
-
     analogWrite(leds[led_on_count], brightness);
 
-    // Serial.println(averaged_value);
     last_time = millis();
+    
+    a2dp_sink.set_volume(volume);
   }
 }
